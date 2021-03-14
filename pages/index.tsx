623f2b0707b0fa967e26bba288c937516a0a4fac
Rafael Angonese <rafael_angonese@unochapeco.edu.br>
2021-03-14 10:50:51 -0300

    adicionado typescript e lint

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
